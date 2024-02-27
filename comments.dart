0e7415450f9ca5b5c6244b78127f529990deda74
Ruby Itimu Magella <160042683+RubyMagella@users.noreply.github.com>
2024-02-27 13:39:37 +0300

    Add files via upload

---
+++ comments.dart
@@ -0,0 +1,11 @@
+void main() {
+  // This is a single line comment- is ignored by compiler
+  /*This is a multi line comment*/
+  /*This is a comment
+  /*This is a nested comment*/end of comment*/
+  //int x = 5;//This line is causing aN error
+
+ // int num = 5;
+  //String name = "Ruby Magella" ;
+  //code won't be executed
+}
