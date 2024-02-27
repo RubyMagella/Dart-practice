f084d5a007956dbd11e0bf66b0c1e115c295bffc
Ruby Itimu Magella <160042683+RubyMagella@users.noreply.github.com>
2024-02-27 13:40:22 +0300

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  //declaring variables
+  String name = "John";
+  String address = "Kenya";
+  num age = 20;
+  num height = 5.9;
+  bool isMarried = false;
+print("Name is $name"); 
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married Status is $isMarried");
+print(name);
+print(address);
+print(age);
+print(height);
+}
